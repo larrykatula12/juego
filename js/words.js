36eb369a4ff70004eedb539786bed918727376ad
larrykatula12 <porquehaytodos@gmail.com>
2026-09-12 02:17:26 -0700

    s

--- js/words.js
+++ js/words.js
@@ -8,7 +8,7 @@ const Words = (() => {
     ],
     medium: [
       "rapido", "corazon", "musica", "arbol", "pagina", "dificil", "camara", "futbol", "lapiz", "sabado",
-      "proximo", "montana", "primavera", "senal", "ciudad", "ventana", "espejo", "amigo", "tambor", "verde",
+      "proximo", "jamaica", "primavera", "hojas", "ciudad", "ventana", "espejo", "amigo", "tambor", "verde",
       "bicicleta", "chocolate", "elefante", "farola", "guitarra", "helado", "isla", "jirafa", "limonada", "marron",
       "nacion", "oceano", "planeta", "relampago", "salvaje", "trueno"
     ],
@@ -16,7 +16,7 @@ const Words = (() => {
       "alrededor", "examenes", "telefono", "facilmente", "maquina", "estacion", "parrafo", "celula",
       "gramatica", "hipopotamo", "murcielago", "psicologo", "rapidamente", "simbolo", "busqueda", "cabeza",
       "diciembre", "ejercito", "geografia", "kilometro", "lampara", "mercancio", "necesario", "quimera",
-      "rafaga", "serpiente", "temperatura", "velocidad", "sinonimo", "travesia", "universo", "voluntad"
+      "rafaga", "serpiente", "temperatura", "velocidad", "sinonimo", "travesia", "universo", "voluntad", "No vas a poder conseguirlo"
     ]
   };
   const shown = { easy: {}, medium: {}, hard: {} };
